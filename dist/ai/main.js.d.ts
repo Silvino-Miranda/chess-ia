570e398c88e8529756753ea68bc7fc430f5ae141
Silvino Miranda <silvino_miranda@hotmail.com>
2021-08-08 16:00:16 -0300

    Update

--- dist/ai/main.js.d.ts
+++
@@ -1,5 +0,0 @@
-declare let aiColor: string;
-declare function ia(): void;
-declare function handleBoard(board: any): Promise<void>;
-declare function generateMoves(board: any, team: any): any;
-declare function generateBoardsData(boardData: any): any[];
--- dist/ai/minimax.js.d.ts
+++
@@ -1,5 +0,0 @@
-declare function maxFun(boardData: any, depth: any): any;
-declare function minFun(boardData: any, depth: any): any;
-declare function minimaxFunAB(boardData: any, isMax: any, alpha: any, beta: any, depth: any): any;
-declare function lessThan(a: any, b: any): boolean;
-declare function biggerThan(a: any, b: any): boolean;
